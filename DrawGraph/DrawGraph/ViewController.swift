9ae9e245b11ce32087b4d0153c8f2db010a0952e
Matthew Van Gorden <33157785+mlvangorden@users.noreply.github.com>
2019-09-13 10:43:04 -0400

    Progress 9/13 10:43am

--- DrawGraph/DrawGraph/ViewController.swift
+++ DrawGraph/DrawGraph/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
+let screenBounds = UIScreen.main.bounds
+let screen_width = screenBounds.width
+let screen_height = screenBounds.height
+let screen_scale = UIScreen.main.scale
+
 class ViewController: UIViewController {
+    
+    let f = GraphView(frame: CGRect(x: 50, y: 50, width: screen_width - 100, height: screen_height - 100))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let childView : UIView = UIView(frame: CGRect(x: 50, y: 50, width: screen_width - 100, height: screen_height - 100) )
+        childView.backgroundColor = UIColor.blue
+        
+        self.view.addSubview(childView)
     }
 
+}
 
+class GraphView: UIView {
+    
+    override func init(frame: CGRect){
+        super.init(frame: frame)
+    }
+    
+    override func drawRect(rect: CGRect) {
+        let
+        
+    }
+    
 }
 
