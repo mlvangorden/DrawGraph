9e73c16db4b9143e736d54cbf3124fbe7733e54d
Matthew Van Gorden <33157785+mlvangorden@users.noreply.github.com>
2019-09-13 11:18:51 -0400

    Progress 9/13 11:20am

--- DrawGraph/DrawGraph/ViewController.swift
+++ DrawGraph/DrawGraph/ViewController.swift
@@ -15,27 +15,39 @@ let screen_scale = UIScreen.main.scale
 
 class ViewController: UIViewController {
     
-    let f = GraphView(frame: CGRect(x: 50, y: 50, width: screen_width - 100, height: screen_height - 100))
+    let f = GraphView(frame: CGRect(x: 0, y: 75, width: screen_width, height: screen_height - 125))
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let childView : UIView = UIView(frame: CGRect(x: 50, y: 50, width: screen_width - 100, height: screen_height - 100) )
-        childView.backgroundColor = UIColor.blue
+        //let childView : UIView = UIView(frame: CGRect(x: 50, y: 50, width: screen_width - 100, height: screen_height - 100) )
+        //childView.backgroundColor = UIColor.blue
         
-        self.view.addSubview(childView)
+        self.view.addSubview(f)
     }
 
 }
 
 class GraphView: UIView {
     
-    override func init(frame: CGRect){
+    override init(frame: CGRect){
         super.init(frame: frame)
     }
+    required init?(coder aDecoder: NSCoder){
+        super.init(coder: aDecoder)
+    }
     
-    override func drawRect(rect: CGRect) {
-        let
+    override func draw(_ rect: CGRect) {
+        let h = rect.height
+        let w = rect.width
+        let color:UIColor = UIColor.purple
         
+        let drect = CGRect(x: (w * 0.25), y: (h * 0.25), width: (w * 0.5), height: (h * 0.5))
+        let bpath:UIBezierPath = UIBezierPath(rect: drect)
+        
+        color.set()
+        bpath.stroke()
+        
+        NSLog("drawRect has updated the view")
     }
     
 }
