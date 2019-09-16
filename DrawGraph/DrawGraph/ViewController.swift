96d63b8e2f510fd38ecfcde5549f1486bdaf96cf
Matthew Van Gorden <33157785+mlvangorden@users.noreply.github.com>
2019-09-16 10:44:36 -0400

    Progress 9/16/19 10:44am

--- DrawGraph/DrawGraph/ViewController.swift
+++ DrawGraph/DrawGraph/ViewController.swift
@@ -15,14 +15,61 @@ let screen_scale = UIScreen.main.scale
 
 class ViewController: UIViewController {
     
-    let f = GraphView(frame: CGRect(x: 0, y: 75, width: screen_width, height: screen_height - 125))
+    let f = GraphView(frame: CGRect(x: 0, y: 50, width: screen_width, height: screen_height - 75))
+    let shape = UIView()
+    var lastPoint = CGPoint.zero
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        //let childView : UIView = UIView(frame: CGRect(x: 50, y: 50, width: screen_width - 100, height: screen_height - 100) )
-        //childView.backgroundColor = UIColor.blue
-        
+        let childView : UIView = UIView(frame: CGRect(x: 0, y: 0, width: screen_width, height: screen_height) )
+        f.backgroundColor = UIColor.white
+        self.view.addSubview(childView)
         self.view.addSubview(f)
+        shape.layer.borderColor = UIColor.black.cgColor
+        shape.backgroundColor = UIColor.clear
+        shape.isHidden = true
+        self.view.addSubview(shape)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            lastPoint = touch.location(in: f)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let currentPoint = touch.location(in: f)
+            reDrawSelectionArea(fromPoint: lastPoint, toPoint: currentPoint)
+        }
+    }
+    
+    func reDrawSelectionArea(fromPoint: CGPoint, toPoint: CGPoint) {
+        shape.isHidden = false
+        
+        //Calculate rect from the original point and last known point
+        let rect = CGRect(x: min(fromPoint.x, toPoint.x),
+                          y: min(fromPoint.y, toPoint.y),
+                          width: abs(fromPoint.x - toPoint.x),
+                          height: abs(fromPoint.y - toPoint.y));
+        
+        shape.frame = rect
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        shape.isHidden = true
+        
+        //User has lift his finger, use the rect
+        let shapeToDraw = shape.frame
+        UIGraphicsPushContext( UIGraphicsGetCurrentContext() ?? <#default value#>)
+        UIColor.blue.setFill()
+        UIRectFill(shapeToDraw)
+        f.draw(shapeToDraw)
+        UIGraphicsPopContext()
+
+        
+        
+        shape.frame = CGRect.zero //reset overlay for next tap
     }
 
 }
@@ -39,15 +86,13 @@ class GraphView: UIView {
     override func draw(_ rect: CGRect) {
         let h = rect.height
         let w = rect.width
-        let color:UIColor = UIColor.purple
+        let color:UIColor = UIColor.black
         
-        let drect = CGRect(x: (w * 0.25), y: (h * 0.25), width: (w * 0.5), height: (h * 0.5))
+        let drect = CGRect(x: w, y: h, width: w, height: h)
         let bpath:UIBezierPath = UIBezierPath(rect: drect)
         
         color.set()
         bpath.stroke()
-        
-        NSLog("drawRect has updated the view")
     }
     
 }
