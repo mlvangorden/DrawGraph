a64af6fb49209a704a3aa5e5f02224e80ab2ee15
Matthew Van Gorden <33157785+mlvangorden@users.noreply.github.com>
2019-09-21 00:46:46 -0400

    Progress 9/21/19 12:46am

--- DrawGraph/DrawGraph/ViewController.swift
+++ DrawGraph/DrawGraph/ViewController.swift
@@ -9,9 +9,39 @@
 import UIKit
 
 let screenBounds = UIScreen.main.bounds
-let screen_width = screenBounds.width
-let screen_height = screenBounds.height
+let screen_width = Int(screenBounds.width)
+let screen_height = Int(screenBounds.height)
 let screen_scale = UIScreen.main.scale
+
+let drawable_top_offset = 150
+let drawable_bottom_offset = 30
+let drawable_height = (screen_height - drawable_top_offset - drawable_bottom_offset)
+
+let colorDict = [UIColor.red.cgColor,
+                 UIColor.orange.cgColor,
+                 UIColor.yellow.cgColor,
+                 UIColor.green.cgColor,
+                 UIColor.blue.cgColor,
+                 UIColor.purple.cgColor,
+                 UIColor.lightGray.cgColor]
+
+let leftEyeDict = ["simple_eye.png",
+                   "happy_left_eye.png",
+                   "angry_left_eye.png",
+                   "sad_left_eye.png",
+                   "tired_left_eye.png"]
+
+let rightEyeDict = ["simple_eye.png",
+                   "happy_right_eye.png",
+                   "angry_right_eye.png",
+                   "sad_right_eye.png",
+                   "tired_right_eye.png"]
+
+let mouthDict = ["simple_mouth.png",
+                 "happy_mouth.png",
+                 "sad_mouth.png",
+                 "happy_jagged_mouth.png",
+                 "upset_jagged_mouth.png"]
 
 class ViewController: UIViewController {
     @IBOutlet weak var originX: UITextField?
@@ -19,95 +49,163 @@ class ViewController: UIViewController {
     @IBOutlet weak var height: UITextField?
     @IBOutlet weak var width: UITextField?
     @IBOutlet weak var draw: UIButton!
+    @IBOutlet weak var positionLabel: UILabel?
+    @IBOutlet weak var heightLabel: UILabel?
+    @IBOutlet weak var widthLabel: UILabel?
     
     var originX_value = -1
     var originY_value = -1
     var height_value = -1
     var width_value = -1
     
-    let f = GraphView(frame: CGRect(x: 0, y: 50, width: screen_width, height: screen_height - 75))
-    var shape = UIView()
+    var colorNum = -1
+    var eyeNum = -1
+    var mouthNum = -1
+    
+    var left_eye = UIImageView()
+    var right_eye = UIImageView()
+    var mouth = UIImageView()
+    
+    let f = UIView(frame: CGRect(x: 0, y: drawable_top_offset, width: screen_width, height: screen_height - drawable_bottom_offset))
     var lastPoint = CGPoint.zero
+    
+    var shape = CAShapeLayer()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        var shape : UIView = UIView(frame: CGRect(x: 0, y: 0, width: 1, height: 1) )
-        let line1 : UIView = UIView(frame: CGRect(x: 0, y: 150, width: screen_width, height: 1) )
-        let line2 : UIView = UIView(frame: CGRect(x: 0, y: screen_height - 30, width: screen_width, height: 1))
+        
+        let line1 : UIView = UIView(frame: CGRect(x: 0, y: drawable_top_offset, width: Int(screen_width), height: 1) )
+        let line2 : UIView = UIView(frame: CGRect(x: 0, y: screen_height - drawable_bottom_offset, width: screen_width, height: 1))
         line1.backgroundColor = UIColor.black
         line2.backgroundColor = UIColor.black
-        //self.view.addSubview(childView)
-        //self.view.insertSubview(f, at: 0)
-        //shape.layer.borderColor = UIColor.black.cgColor
-        //shape.backgroundColor = UIColor.clear
-        //shape.isHidden = true
         self.view.addSubview(line1)
         self.view.addSubview(line2)
-        
     }
     
-    /*override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    @IBAction func draw_rectangle(_ sender: UIButton){
+        if((originX!.text != "") && (originY!.text != "") && (height!.text != "") && (width!.text != "")){
+            //keeps parameters within the proper bounds
+            originX_value = min((Int(originX!.text!) ?? -1), screen_width - 10)
+            originY_value = min((Int(originY!.text!) ?? -1), drawable_height - 10)
+            height_value = min((Int(height!.text!) ?? -1), (drawable_height - originY_value))
+            width_value = min((Int(width!.text!) ?? -1), (screen_width - originX_value))
+            
+            colorNum = Int.random(in: 0..<7)
+            eyeNum = Int.random(in: 0..<5)
+            mouthNum = Int.random(in: 0..<5)
+            
+            createHead(color_number: colorNum)
+            createFace(eye_number: eyeNum, mouth_number: mouthNum)
+        }
+        else{
+            resetFriend()
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
+            resetFriend()
+            
             lastPoint = touch.location(in: f)
+            originX_value = Int(lastPoint.x)
+            originY_value = Int(lastPoint.y)
+            
+            if(originY_value < 0) {
+                originY_value = 0
+            }
+            else if(originY_value > drawable_height){
+                originY_value = drawable_height - 10
+            }
+            
+            originX?.text = String(originX_value)
+            originY?.text = String(originY_value)
+            colorNum = Int.random(in: 0..<7)
+            eyeNum = Int.random(in: 0..<5)
+            mouthNum = Int.random(in: 0..<5)
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         if let touch = touches.first {
             let currentPoint = touch.location(in: f)
-            reDrawSelectionArea(fromPoint: lastPoint, toPoint: currentPoint)
+            if(currentPoint.x < lastPoint.x) {
+                originX_value = Int(currentPoint.x)
+                width_value = Int(lastPoint.x) - originX_value
+            }
+            else{
+                width_value = Int(currentPoint.x) - originX_value
+            }
+            if(currentPoint.y < lastPoint.y) {
+                originY_value = max(Int(currentPoint.y), 0)
+                height_value = Int(lastPoint.y) - originY_value
+            }
+            else{
+                height_value = min((Int(currentPoint.y) - originY_value), (drawable_height - originY_value - 1))
+            }
+            createHead(color_number: colorNum)
+            createFace(eye_number: eyeNum, mouth_number: mouthNum)
+            
         }
     }
     
-    func reDrawSelectionArea(fromPoint: CGPoint, toPoint: CGPoint) {
-        shape.isHidden = false
+    func createHead(color_number: Int) {
+        originX?.text = String(originX_value)
+        originY?.text = String(originY_value)
+        width?.text = String(width_value)
+        height?.text = String(height_value)
         
-        //Calculate rect from the original point and last known point
-        let rect = CGRect(x: min(fromPoint.x, toPoint.x),
-                          y: min(fromPoint.y, toPoint.y),
-                          width: abs(fromPoint.x - toPoint.x),
-                          height: abs(fromPoint.y - toPoint.y));
+        if(height_value < 10) {
+            height_value = 10
+        }
+        if(width_value < 10) {
+            width_value = 10
+        }
+        if(originX_value > screen_width - 10) {
+            originX_value = screen_width - 10
+        }
+        if(originY_value > drawable_height - 10) {
+            originY_value = drawable_height - 10
+        }
         
-        shape.frame = rect
+        shape.fillColor = colorDict[color_number]
+        shape.path = UIBezierPath(roundedRect: CGRect(x: originX_value, y: originY_value + drawable_top_offset + 1, width: width_value, height: height_value), cornerRadius: 50).cgPath
+        view.layer.addSublayer(shape)
     }
     
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        shape.isHidden = true
+    func createFace(eye_number: Int, mouth_number: Int) {
+        let eyeHeight = min( (CGFloat(height_value) / 3), (CGFloat(width_value) / 3) )
+        let eyeWidth = eyeHeight
+        let mouthHeight = eyeHeight
+        let mouthWidth = 2 * eyeHeight
         
-        //User has lift his finger, use the rect
-        let shapeToDraw = shape.frame
-        UIColor.blue.setFill()
-        UIRectFill(shapeToDraw)
-        f.draw(shapeToDraw)
-        UIGraphicsPopContext()
-
+        let eyeLX = ( (CGFloat(width_value) / 3) - (eyeWidth / 2) ) + CGFloat(originX_value)
+        let eyeLY = ( (CGFloat(height_value) / 3) - (eyeHeight / 2) ) + CGFloat(originY_value + drawable_top_offset)
+        let eyeRX = ( (2 * CGFloat(width_value) / 3) - (eyeWidth / 2) ) + CGFloat(originX_value)
+        let eyeRY = eyeLY
+        let mouthX = ( (CGFloat(width_value) / 2) - (mouthWidth / 2) ) + CGFloat(originX_value)
+        let mouthY = ( 0.55 * CGFloat(height_value) ) + CGFloat(originY_value + drawable_top_offset)
         
+        left_eye.frame = CGRect(x: eyeLX, y: eyeLY, width: eyeWidth, height: eyeHeight)
+        right_eye.frame = CGRect(x: eyeRX, y: eyeRY, width: eyeWidth, height: eyeHeight)
+        mouth.frame = CGRect(x: mouthX, y: mouthY, width: mouthWidth, height: mouthHeight)
         
-        shape.frame = CGRect.zero //reset overlay for next tap
-    }*/
+        left_eye.image = UIImage(named: leftEyeDict[eye_number])
+        right_eye.image = UIImage(named: rightEyeDict[eye_number])
+        mouth.image = UIImage(named: mouthDict[mouth_number])
+        
+        view.addSubview(left_eye)
+        view.addSubview(right_eye)
+        view.addSubview(mouth)
+    }
+    
+    func resetFriend() {
+        left_eye.image = nil
+        right_eye.image = nil
+        mouth.image = nil
+        shape.fillColor = nil
+        shape.frame = CGRect(x: 0, y: 0, width: 0, height: 0)
+        width?.text = ""
+        height?.text = ""
+    }
 
 }
-
-class GraphView: UIView {
-    
-    override init(frame: CGRect){
-        super.init(frame: frame)
-    }
-    required init?(coder aDecoder: NSCoder){
-        super.init(coder: aDecoder)
-    }
-    
-    override func draw(_ rect: CGRect) {
-        let h = rect.height
-        let w = rect.width
-        let color:UIColor = UIColor.black
-        
-        let drect = CGRect(x: w, y: h, width: w, height: h)
-        let bpath:UIBezierPath = UIBezierPath(rect: drect)
-        
-        color.set()
-        bpath.stroke()
-    }
-    
-}
-
